9d74459f7765571c5e18560a91bcde619efaa96e
george bres <geobres@gmail.com>
2021-09-15 01:01:09 +0300

    edit the mega menu with hover

--- jstools/tailwind.config.js
+++ jstools/tailwind.config.js
@@ -13,10 +13,54 @@ module.exports = {
   theme: {
       extend: {
         spacing: {
-          '120': '28rem',
-          '144': '32rem',
-          '168': '36rem',
+          '108': '27rem',
+          '112': '28rem',
+          '120': '30rem',
+          '128': '32rem',
+          '136': '34rem',
+          '144': '36rem',
+          '152': '38rem',
           '192': '48rem',
+          '200': '50rem',
+        }
+      },
+  },
+  variants: {
+    // The 'active' variant will be generated in addition to the defaults
+    extend: {
+      display: ['group-hover'],
+      transitionDuration: ['group-hover'],
+      transitionProperty: ['group-hover' ]
+
+    }
+  },
+  plugins: [],
+}
+module.exports = {
+  future: {
+      removeDeprecatedGapUtilities: true,
+      purgeLayersByDefault: true,
+  },
+  purge: {
+      enabled: false, //true for production build
+      content: [
+          '../**/templates/*.html',
+          '../**/templates/**/*.html'
+      ]
+  },
+  theme: {
+      extend: {
+        spacing: {
+          '108': '27rem',
+          '112': '28rem',
+          '116': '29rem',
+          '120': '30rem',
+          '128': '32rem',
+          '136': '34rem',
+          '144': '36rem',
+          '152': '38rem',
+          '192': '48rem',
+          '200': '50rem',
         }
       },
   },
