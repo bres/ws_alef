b4795d9ed97bf7e91a46fdcb1686a32de96764e8
george bres <geobres@gmail.com>
2021-12-07 16:59:00 +0200

    edit the checkout page

--- orders/models.py
+++ orders/models.py
@@ -44,8 +44,7 @@ class Order(models.Model):
     is_ordered = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    free_delivery=models.BooleanField(default=True)
-    store_pickup=models.BooleanField(default=False)
+    #shipping_method=models.BooleanField()
 
 
     def full_name(self):
