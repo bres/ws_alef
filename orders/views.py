cb735d3190f95acc0301794ba4a89be86f9b7f12
george bres <geobres@gmail.com>
2021-12-10 12:44:51 +0200

    finish send transaction to backend

--- orders/views.py
+++ orders/views.py
@@ -1,21 +1,40 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from carts.models import CartItem
 from .forms import OrderForm
 import datetime
-from .models import Order
+from .models import Order, Payment
+import json
+
 
 def payments(request):
-    return render(request,'orders/payments.html',)
+    body = json.loads(request.body)
+    print(body)
+    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
 
 
+    # Store transaction details inside Payment model
+    payment = Payment(
+        user=request.user,
+        payment_id=body['transID'],
+        payment_method=body['payment_method'],
+        amount_paid=order.order_total,
+        status=body['status'],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
+
+    return render(request, 'orders/payments.html', )
 
 
 def place_order(request, total=0, quantity=0):
-    current_user=request.user
-    #id the cart count is less or equal then reidrect to store list of items
-    cart_items =CartItem.objects.filter(user=current_user)
-    cart_count=cart_items.count()
+    current_user = request.user
+    # id the cart count is less or equal then reidrect to store list of items
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('store:product_list')
 
@@ -52,8 +71,8 @@ def place_order(request, total=0, quantity=0):
             yr = int(datetime.date.today().strftime('%Y'))
             dt = int(datetime.date.today().strftime('%d'))
             mt = int(datetime.date.today().strftime('%m'))
-            d = datetime.date(yr,mt,dt)
-            current_date = d.strftime("%Y%m%d") #20210305
+            d = datetime.date(yr, mt, dt)
+            current_date = d.strftime("%Y%m%d")  # 20210305
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
@@ -63,10 +82,10 @@ def place_order(request, total=0, quantity=0):
                 'order': order,
                 'cart_items': cart_items,
                 'total': total,
-                'tax': tax, #optional
+                'tax': tax,  # optional
                 'grand_total': grand_total,
             }
-            return render(request,'orders/payments.html',context)
+            return render(request, 'orders/payments.html', context)
 
     else:
         return redirect('carts:checkout')
