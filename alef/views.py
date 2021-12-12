1c4306674c37197d26e1e1dc941f09033b09d241
george bres <geobres@gmail.com>
2021-12-13 01:22:31 +0200

    finish generatetion of invoice on order completion

--- alef/views.py
+++ alef/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ContactForm
+from store.models import Product
 # Create your views here.
 
 def home(request):
     #return HttpResponse('this is the home page')
-    return render(request,'homepage.html')
+    products = Product.objects.filter().order_by('-created_date')[0:4]
+    context={
+            'products': products,
+
+    }
+    return render(request,'homepage.html',context)
 
 def contact(request):
     contact_form=ContactForm(request.POST or None)
