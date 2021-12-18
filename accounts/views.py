2b2829ba7c9182f028282465d3ba32c27d4c1eca
george bres <geobres@gmail.com>
2021-12-18 18:02:01 +0200

    adding new requirements

--- accounts/views.py
+++ accounts/views.py
@@ -19,38 +19,6 @@ import requests
 
 #Create your views here.
 
-# def signup_view(request):
-#     if request.method =="POST" :
-#         account_form= UserCreationForm(request.POST)
-#         if account_form.is_valid():
-#             account_form.save()
-#             user = account_form.save() # i do not need get because user is inside on the form
-#             login(request,user)   #log the user in
-#             return redirect("home")
-#     else: #if it is a get request
-#         account_form = UserCreationForm() # creating a fresh blank form
-#     return render(request,'accounts/signup.html',{'account_form':account_form})
-#
-# def login_view(request):
-#     if request.method == 'POST':
-#         account_form = AuthenticationForm(data=request.POST)
-#         if account_form.is_valid():
-#             user = account_form.get_user()
-#             login(request,user)  #log the user in
-#             if 'next' in request.POST:
-#                 return redirect(request.POST.get('next'))
-#             else:
-#                 return redirect("home")
-#     else: #if it is a get request
-#         account_form = AuthenticationForm()
-#     return render(request,'accounts/login.html',{'account_form':account_form})
-#
-#
-# def logout_view(request):
-#     if request.method == 'POST':
-#         logout(request) #log out the user
-#         return redirect('accounts:login')
-#     return render(request,'accounts/logout.html')
 
 
 def register(request):
