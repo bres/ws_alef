dd516c5afb290a295c856a89e8d10a506e4ea31a
agreatdev <agreatdev@agreatdevs-iMac.local>
2022-01-07 20:02:40 +0200

    add allowed host

--- alef/settings.py
+++ alef/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = ['wsalef.herokuapp.com', '127.0.0.1', 'alefatelier.com']
+ALLOWED_HOSTS = ['wsalef.herokuapp.com', '127.0.0.1', 'www.alefatelier.com']
 
 # Application definition
 
