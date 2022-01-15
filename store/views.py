8a05b1c34eb3ce7a58bbbe0e3c9196e555520524
agreatdev <agreatdev@agreatdevs-iMac.local>
2022-01-15 21:42:13 +0200

    change number of items in list to 8

--- store/views.py
+++ store/views.py
@@ -30,7 +30,7 @@ def product_list(request, category_slug=None):
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True).order_by(temp)
-        paginator = Paginator(products, 6)
+        paginator = Paginator(products, 8)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
