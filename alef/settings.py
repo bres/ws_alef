91b561f15dc1d4d56e3ed89313db560b88a28b63
agreatdev <agreatdev@agreatdevs-iMac.local>
2022-01-16 23:52:55 +0200

    comment out staticfiles storage whitenoise

--- alef/settings.py
+++ alef/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # new
 
 # # MEDIA_URL is the reference URL for browser to access the files over Http.
 # MEDIA_URL = '/media/'
